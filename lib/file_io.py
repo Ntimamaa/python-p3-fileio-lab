5cce362aed8572d61ceb2d0e19e666b773821bb3
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-08-30 10:52:26 +0300

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name)
+    with open(file_name + '.txt', 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name)
+    with open(file_name + '.txt', 'a') as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name)
+    with open(file_name + '.txt', 'r') as f:
+        return f.read()
+
+
+write_file(file_name="file1", file_content="This is a test content.")
+append_file(file_name="file2", append_content="This is a test content.")
+
+print(read_file(file_name="logfile"))
+
